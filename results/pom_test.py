ffe487c1cd526329fb4e36f749919a7173c9ceb8
TonikaHristova <hristovatonika@gmail.com>
2018-10-30 16:05:25 +0200

    udemy

---
+++ results/pom_test.py
@@ -0,0 +1,24 @@
+# The test
+from selenium import webdriver
+from pages.training_ground_page import TrainingGroundPage
+from pages.trial_page import TrialPage
+
+browser = webdriver.Chrome()
+
+#Trial Test
+trial_page = TrialPage(driver=browser)
+trial_page.go()
+trial_page.stone_input.input_text("rock")
+trial_page.stone_button.click()
+
+input()
+#Training Ground
+trng_page = TrainingGroundPage(driver=browser)
+trng_page.go()
+
+assert trng_page.button1.text == "Button1", "Unexpected button1 text"
+print("Test passed")
+
+input()
+
+browser.quit()
